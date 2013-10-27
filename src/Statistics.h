33a82d82fce0c162e2a8cb222285daa8b8893d14
Andrew Moreland <andy@andymo.org>
2013-10-27 03:17:38 -0700

    Adding additional methods to statistics interface

--- src/Statistics.h
+++ src/Statistics.h
@@ -12,9 +12,12 @@ namespace Shipping {
   class Statistics : Entity<Statistics> {
   public:
     typedef Fwk::Ptr<Statistics> Ptr;
+    typedef float Percentage;
 
     EntityCount locationType(Location::Type type) { return locationStats_[type]; }
     EntityCount segmentType(Segment::Type type) { return segmentStats_[type]; }
+
+    Percentage expeditedShippingPercentage();
 
   private:
     Statistics(EntityName name) : Entity(name) {}
