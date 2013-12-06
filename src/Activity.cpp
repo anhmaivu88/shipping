2b02e3515deeec2905a603f830014d382fae9a04
Andrew Moreland <andy@andymo.org>
2013-12-05 16:50:59 -0800

    working on activity

--- src/Activity.cpp
+++ src/Activity.cpp
@@ -3,10 +3,25 @@
 
 namespace Shipping {
   class ActivityImpl : public Activity {
-    void notifieeAdd(Notifiee *notifiee) { notifiees_.push_back(notifiee); }
-    void notifieeDel(Notifiee *notifiee) { notifiees_.erase(find(notifiees_.begin(), notifiees_.end(), notifiee)); }
-
+    virtual void notifieeAdd(Notifiee *notifiee);
+    virtual void notifieeDel(Notifiee *notifiee);
+    virtual void statusIs(Status status);
+    virtual Status status();
+    virtual void nextTimeIs();
+    virtual Time nextTime();
+    
   private:
     std::vector<Notifiee::Ptr> notifiees_;
+    Status status_;
+    Time nextTime_;
   };
+
+  void ActivityImpl::notifieeAdd(Notifiee *notifiee) { notifiees_.push_back(notifiee); }
+  void ActivityImpl::notifieeDel(Notifiee *notifiee) { notifiees_.erase(find(notifiees_.begin(), notifiees_.end(), notifiee)); }
+
+  void ActivityImpl::statusIs(Status s) { }
+  Status ActivityImpl::status() { return status_; }
+
+  void ActivityImpl::nextTimeIs(Time t) { nextTime_ = t; }
+  Time ActivityImpl::nextTime() { return nextTime_; }
 }
