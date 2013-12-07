6c367c0ae30d7a30ca2779b57e043cb18c828ca1
Leo Martel <lmartel@github>
2013-12-06 21:30:33 -0800

    README update and attempt at fixing experiment (didn't work)

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -21,36 +21,53 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // std::vector<Ptr<Instance>> sources;
-  std::vector<Ptr<Instance>> deliverySegments;
   std::vector<Ptr<Instance>> allSegments;
+
+  // Connect customer to root terminal
   Ptr<Instance> destination = manager->instanceNew("destination", "Customer");
   Ptr<Instance> root = manager->instanceNew("rootTerminal", "Truck terminal");
+
+  Ptr<Instance> deliverySegment = manager->instanceNew("deliverySeg", "Truck segment");
+  deliverySegment->attributeIs("source", root->name());
+
+  Ptr<Instance> deliveryBak = manager->instanceNew("deliveryBak", "Truck segment");
+  deliveryBak->attributeIs("source", destination->name());
+
+  deliverySegment->attributeIs("return segment", deliveryBak->name());
+
+  allSegments.push_back(deliverySegment);
+  allSegments.push_back(deliveryBak);
+
   for(int i = 0; i < 10; i++){
     // Connect terminal to root
     Ptr<Instance> terminal = manager->instanceNew("terminal" + str(i), "Truck terminal");
     Ptr<Instance> segment = manager->instanceNew("seg" + str(i), "Truck segment");
+    segment->attributeIs("length", "1");
     segment->attributeIs("source", root->name());
 
     Ptr<Instance> backseg = manager->instanceNew("backseg" + str(i), "Truck segment");
+    backseg->attributeIs("length", "1");
     backseg->attributeIs("source", terminal->name());
 
     segment->attributeIs("return segment", backseg->name());
-    deliverySegments.push_back(segment);
     allSegments.push_back(segment);
+    allSegments.push_back(backseg);
 
     // Connect sources to terminal
     for(int j = 0; j < 10; j++){
       Ptr<Instance> source = manager->instanceNew("source" + str(i) + "," + str(j), "Customer");
       Ptr<Instance> sourceSegment = manager->instanceNew("seg" + str(i) + "," + str(j), "Truck segment");
+      sourceSegment->attributeIs("length", "1");
       sourceSegment->attributeIs("source", source->name());
 
       Ptr<Instance> sourceBackseg = manager->instanceNew("backseg" + str(i) + "," + str(j), "Truck segment");
+      sourceBackseg->attributeIs("length", "1");
       sourceBackseg->attributeIs("source", terminal->name());
 
       sourceSegment->attributeIs("return segment", sourceBackseg->name());
 
       allSegments.push_back(sourceSegment);
+      allSegments.push_back(sourceBackseg);
 
       // Configure sources
       if(argc > 1 && std::string(argv[1]) == "--randomize"){
@@ -73,16 +90,13 @@ int main(int argc, char *argv[]) {
   
   cout << "Done!" << endl;
   
-  cout << "Received shipments: " << destination->attribute("Shipments Received") << endl;
-  double deliverRefused = 0.0;
-  for(Ptr<Instance> seg : deliverySegments){
-    deliverRefused += atof(seg->attribute("Shipments Refused").c_str());
-  }
+  cout << "Received shipments: " << deliverySegment->attribute("Shipments Received") << endl;
+
   double totalRefused = 0.0;
   for(Ptr<Instance> seg : allSegments){
     totalRefused += atof(seg->attribute("Shipments Refused").c_str());
   }
-  cout << "Refused shipments (delivery segments) : " << deliverRefused << endl;
+  cout << "Refused shipments (delivery segment) : " << deliverySegment->attribute("Shipments Refused") << endl;
   cout << "Refused shipments (all segments) : " << totalRefused << endl;
   cout << "Delivered shipments: " << destination->attribute("Shipments Received") << endl;
   cout << "Average latency: " << destination->attribute("Average Latency") << endl;
